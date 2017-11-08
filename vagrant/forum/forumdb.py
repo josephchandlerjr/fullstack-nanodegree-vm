135a87511d928d5cf84d1b5e17591f39c5091320
Joseph Alan Chandler Jr <josephalanchandlerjr@gmail.com>
2017-11-08 10:09:48 -0500

    initial commit

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,29 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
+import bleach
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+DBNAME = "forum"
+
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  conn = psycopg2.connect(dbname=DBNAME)
+  c = conn.cursor()
+  c.execute("select content, time from posts order by time desc;")
+  posts = c.fetchall()
+  conn.close()
+  return posts
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+  content = bleach.clean(content)
+  conn = psycopg2.connect(dbname=DBNAME)
+  c = conn.cursor()
+  c.execute("insert into posts values (%s)", (content,))
+  conn.commit()
+  conn.close()
+
 
 
